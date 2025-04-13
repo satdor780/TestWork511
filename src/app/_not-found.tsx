c6ba6018a5a4c70148cc7d3a2ab2d6b626e61679
fogxx <you@example.com>
2025-04-14 04:14:56 +0500

    add not found page

---
+++ src/app/_not-found.tsx
@@ -0,0 +1,10 @@
+import { Emptiness } from '@/components/Emptiness/Emptiness';
+
+export default function NotFound() {
+  return (
+    <Emptiness
+      title="Страница не найдена"
+      subtitle="К сожалению, запрашиваемая страница не существует."
+    />
+  );
+}
