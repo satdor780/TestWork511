8c771b4e0bd1efdba9f5640936d3e0651e8d228e
fogxx <you@example.com>
2025-04-14 05:20:14 +0500

    fixxxxxxx

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,12 +8,14 @@ const schema = Joi.object({
 
 const { value, error } = schema.validate(
   {
-    NEXT_PUBLIC_WEATHER_API_URL: process.env.NEXT_PUBLIC_WEATHER_API_URL,
-    NEXT_PUBLIC_GEO_API_URL: process.env.NEXT_PUBLIC_GEO_API_URL,
-    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    NEXT_PUBLIC_WEATHER_API_URL:
+      process.env.NEXT_PUBLIC_WEATHER_API_URL || 'https://api.openweathermap.org/data/2.5',
+    NEXT_PUBLIC_GEO_API_URL:
+      process.env.NEXT_PUBLIC_GEO_API_URL || 'https://api.openweathermap.org/geo/1.0',
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY || '12ec3237653c969992ec83fda3d19fbe',
   },
   { stripUnknown: true },
-);
+); // fix me
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
